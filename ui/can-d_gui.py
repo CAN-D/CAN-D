8094a682ddf73cb7fbf738ec51cd84ed2c54a3e6
enzozafra <zafra@ualberta.ca>
2019-01-31 10:06:18 -0700

    Init UI

--- ui/can-d_gui.py
+++ ui/can-d_gui.py
@@ -2,12 +2,16 @@ import sys
 import random
 from PySide2 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QWidget):
+class CAND(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.title = 'CAN-D Automotive Datalogger'
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.initializeUI()
 
+    def initializeUI(self):
+        self.setWindowTitle(self.title)
         self.button = QtWidgets.QPushButton("Click me!")
         self.text = QtWidgets.QLabel("Hello World")
         self.text.setAlignment(QtCore.Qt.AlignCenter)
@@ -18,18 +22,18 @@ class MyWidget(QtWidgets.QWidget):
         self.setLayout(self.layout)
 
         self.button.clicked.connect(self.magic)
+        
+        self.showMaximized()
 
     def magic(self):
         text = random.choice(self.hello)
         self.text.setText(text)
         self.text.repaint()
-        print(text)
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    cand = CAND()
 
     sys.exit(app.exec_())
