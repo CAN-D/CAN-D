061de8a831df3484e64a9ebe9ca5584b3e6ef9a3
enzozafra <zafra@ualberta.ca>
2019-02-15 14:54:48 -0700

    Horizontal expander done, need vertical expander

--- gooey/gooey/can-d_gui.py
+++ gooey/gooey/can-d_gui.py
@@ -2,6 +2,7 @@ import sys
 import random
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
+from ui.Expander import Expander
 
 class CAND(QWidget):
     def __init__(self):
@@ -20,10 +21,20 @@ class CAND(QWidget):
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
+
+        self.layout2 = QVBoxLayout()
+        self.layout2.addWidget(self.text)
+        self.layout2.addWidget(self.button)
+        self.setLayout(self.layout2)
+
+        self.expander = Expander();
+        self.expander.setContentLayout(self.layout2);
+        self.layout.addWidget(self.expander)
         self.setLayout(self.layout)
 
         self.button.clicked.connect(self.magic)
-        
+
+
         self.showMaximized()
 
     def magic(self):
