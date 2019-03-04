7822ad49e89e4be0ab73235b30cd3c8f2bcf050c
Mark Griffith <markygriff12@gmail.com>
2019-03-03 19:55:59 -0700

    Add GDB host script

---
+++ CAN-D/ST-Link/arm-gdb_host.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PORT=4422
+ELF=""
+
+setup()
+{
+  shopt -s nullglob
+
+  files=( *.elf )
+  if [ ${#files[@]} -gt 1 ]; then
+    echo "Found more than one elf file. Exiting..."
+    return 1
+  fi
+  if [ ${#files[@]} -eq 0 ]; then
+    echo "No elf file found. Exiting..."
+    return 1
+  fi
+
+  ELF=${files[0]}
+  echo "Using ${ELF}"
+
+  return 0
+}
+
+setup
+/usr/bin/expect ${HOME}/.scripts/arm-gdb_scripts/arm-gdb_load ${ELF}
+exit 0
+
