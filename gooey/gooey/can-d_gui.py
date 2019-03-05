f6711a0d7b5da835cf2c365f244b31627abb165e
enzozafra <zafra@ualberta.ca>
2019-03-05 09:31:48 -0700

    Restructure gooey project

--- gooey/gooey/can-d_gui.py
+++ gooey/gooey/can-d_gui.py
@@ -1,51 +1,39 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
-from ui.Expander import Expander
+from ui.widgets.mainwindow import CAND_MainWindow
+from ui.widgets.Expander import Expander
 
-class CAND(QWidget):
+
+class CAND(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.title = 'CAN-D Automotive Datalogger'
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        self.initializeUI()
 
     def initializeUI(self):
         self.setWindowTitle(self.title)
-        self.button = QPushButton("Click me!")
-        self.text = QLabel("Hello World")
-        self.text.setAlignment(Qt.AlignCenter)
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        CAND_MainWindow(self)
 
-        self.layout2 = QVBoxLayout()
-        self.layout2.addWidget(self.text)
-        self.layout2.addWidget(self.button)
-        self.setLayout(self.layout2)
+        # self.layout = QVBoxLayout()
+        # self.layout2 = QVBoxLayout()
+        # # Test
+        # self.setLayout(self.layout2)
 
-        self.expander = Expander();
-        self.expander.setContentLayout(self.layout2);
-        self.layout.addWidget(self.expander)
-        self.setLayout(self.layout)
-
-        self.button.clicked.connect(self.magic)
-
+        # self.expander = Expander()
+        # self.expander.setContentLayout(self.layout2)
+        # self.layout.addWidget(self.expander)
+        # self.setLayout(self.layout)
 
         self.showMaximized()
-
-    def magic(self):
-        text = random.choice(self.hello)
-        self.text.setText(text)
-        self.text.repaint()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     cand = CAND()
+    cand.initializeUI()
 
     sys.exit(app.exec_())
