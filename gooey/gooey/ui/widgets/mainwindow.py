faa2767adaac5a16b92c845b2714ac31133ffc91
enzozafra <zafra@ualberta.ca>
2019-03-17 21:35:28 -0600

    Show message test

--- gooey/gooey/ui/widgets/mainwindow.py
+++ gooey/gooey/ui/widgets/mainwindow.py
@@ -248,16 +248,17 @@ class CAND_MainWindow(QMainWindow):
 
     # TODO: REMOVE THESE, ONLY FOR TEST
     def insertTransmit(self):
-        print("testTransmit")
         newmsg = TransmitMessage(
             "TestTransmit", "DLC", "Data", "cycle_time", "Count", "Trigger")
         self.rxtxTab.appendTransmitTable(newmsg)
+        self.statusbar.showMessage("Test Transmit")
 
     def insertReceive(self):
         print("testReceive")
         newmsg = ReceiveMessage(
             "TestReceive", "DLC", "Data", "cycle_time", "Count")
         self.rxtxTab.appendReceiveTable(newmsg)
+        self.statusbar.showMessage("Test Receive")
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
