51b45fce88f6eacad49d6f9e01310a8bdd5eb521
enzozafra <zafra@ualberta.ca>
2019-03-19 11:19:31 -0600

    Transmit Popup window

--- gooey/gooey/ui/widgets/autogen/mainwindow.py
+++ gooey/gooey/ui/widgets/autogen/mainwindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/qtdesigner/mainwindow.ui'
+# Form implementation generated from reading ui file 'qtdesigner/mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.12
 #
