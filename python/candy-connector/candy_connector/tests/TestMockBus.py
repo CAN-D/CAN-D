2fddefd8d8155114cfed0a62e52f473695bc4b94
Brad Ofrim <bofrim@gmail.com>
2019-03-30 20:07:26 -0600

    Updates the scripts so they're actually async.

--- python/candy-connector/candy_connector/tests/TestMockBus.py
+++ python/candy-connector/candy_connector/tests/TestMockBus.py
@@ -1,9 +1,10 @@
 from candy_connector.CanDBus import CannedBus
 import candy_connector.proto.can_d_pb2 as pb
+import logging
 
 
 bus = CannedBus(log_path="./data/can_trace_255.log.test")
 
 while True:
-    data = bus.recv()
-    print(f"Got message: {data}")
+    msg = bus.recv()
+    print(f"Got message: {msg}")
